d677e10870d4449ee3178dc17cc0197db8668c3b
josenunez <josenunez@outlook.de>
2024-05-12 10:52:40 +0200

    refactor offlinehunter test

--- tests/offlinehunter.js
+++ tests/offlinehunter.js
@@ -4,10 +4,7 @@ import fs from "fs";
 function offlineHunt() {
     const data = JSON.parse(fs.readFileSync("./tests/fixtures/mock_data.json", "utf8"));
 
-    const a = df.findShopMostCards(data);
-
-    const output = JSON.stringify(a, null, 2);
-    console.log(output);
+    df.findShopMostCards(data);
 }
 
 offlineHunt()
