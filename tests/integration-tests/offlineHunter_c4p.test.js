0573fd63926b942a09f7b01625dd81a26bc78b95
josenunez <josenunez@outlook.de>
2024-05-27 10:34:51 +0200

    clean up test for 4 items

--- tests/integration-tests/offlineHunter_c4p.test.js
+++ tests/integration-tests/offlineHunter_c4p.test.js
@@ -1,5 +1,4 @@
 const offlineHunter = require("/Users/josenunez/Documents/repo/cardhunter/src/offlinehunter.js");
-const {convertToJson} = require("../utils/testUtils");
 const {complete} = require("../utils/expectedResults/expectedResults_c4p.js");
 
 describe('test purchase 4 Item Combination considering price and delivery fee', () => {
@@ -10,28 +9,4 @@ describe('test purchase 4 Item Combination considering price and delivery fee', 
 
         expect(output.trim()).toBe(complete);
     });
-
-    // it('should consider buying a more expensive card to reduce total costs', async () => {
-    //     const output = await offlineHunter(filePath);
-    //
-    //     expect(output.trim()).toContain(moreExpensiveItemToReduceTotalCost);
-    // });
-    //
-    // it('should show multiple combinations considering the same shops', async () => {
-    //     const output = await offlineHunter(filePath);
-    //     const outputTrimmed = output.trim();
-    //
-    //     expect(outputTrimmed).toContain(bestCombinationSameShop);
-    //     expect(outputTrimmed).toContain(secondBestCombinationSameShop);
-    // });
-    //
-    // it('should verify 4 number of combinations ordered by price ASC', async () => {
-    //     const output = await offlineHunter(filePath);
-    //     const recommendationObj = convertToJson(output);
-    //
-    //     expect(recommendationObj).toHaveLength(4);
-    //
-    //     const totalCosts = recommendationObj.map(combination => combination.totalCostWithDelivery);
-    //     expect(totalCosts).toEqual([...totalCosts].sort((a, b) => a - b));
-    // });
 });
