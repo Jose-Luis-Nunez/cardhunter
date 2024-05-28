fe8aeb86358ee2367dd446aefec4b51a73a70ba3
josenunez <josenunez@outlook.de>
2024-05-28 12:36:44 +0200

    add time to check where to improve

--- src/offlineHunter.js
+++ src/offlineHunter.js
@@ -5,14 +5,35 @@ const ShopOutputService = require("./services/cardsrecommendation/ShopOutputServ
 
 async function offlineHunter(filePath, topN = 4) {
     try {
+        console.time('Total time');
+
+        console.time('Reading card data');
         const cardData = CardDataService.readCardDataFromFile(filePath);
+        console.timeEnd('Reading card data');
+
+        console.time('Generating product options');
         const productOptions = ShopPurchaseOptionsService.generateProductOptions(cardData);
+        console.timeEnd('Generating product options');
+
+        console.time('Finding optimal combinations');
         const optimalCombinations = ShopPurchaseOptionsService.findOptimalCombinations(productOptions, topN);
+        console.timeEnd('Finding optimal combinations');
+
+        console.time('Calculating costs');
         const costs = CostCalculationService.calculateCosts(optimalCombinations);
-        return ShopOutputService.printTopCombinations(costs);
+        console.timeEnd('Calculating costs');
+
+        console.time('Printing top combinations');
+        const result = ShopOutputService.printTopCombinations(costs);
+        console.timeEnd('Printing top combinations');
+
+        console.timeEnd('Total time');
+
+        return result;
     } catch (error) {
         console.error("Error during offline hunt:", error);
     }
 }
+
 
 module.exports = offlineHunter;
